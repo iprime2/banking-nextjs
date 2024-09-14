02a03f3471b18f5a264361a300130c6f144a1c45
Sushil Kumar Gupta <kalwarsushil2@gmail.com>
2024-09-14 17:17:27 +0530

    Implement Complete Layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,29 @@
+import Image from "next/image";
+
+// Components
+import MobileNav from "@/components/MobileNav";
+import Sidebar from "@/components/Sidebar";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = {firstName: "Sushil", lastName: "Gupta", email: "kalwarsushil2@gmail.com"};
+
   return (
-    <main>
-        SIDEBAR
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
+
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
         {children}
+      </div>
     </main>
   );
 }
