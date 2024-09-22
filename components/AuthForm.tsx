796468c895cb57e6a295f3a199bff1ae96e41661
Sushil Kumar Gupta <kalwarsushil2@gmail.com>
2024-09-22 17:25:57 +0530

    Implement Plaid Link and Dwollia

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -15,6 +15,7 @@ import { authFormSchema } from '@/lib/utils'
 import { Loader2 } from 'lucide-react';
 import { useRouter } from 'next/navigation'
 import { signIn, signUp } from '@/lib/actions/user.actions'
+import PlaidLink from './PlaidLink'
 
 
 const AuthForm = ({type}: {type: string}) => {
@@ -36,9 +37,22 @@ const AuthForm = ({type}: {type: string}) => {
     setIsLoading(true);
     try {
         if(type === 'sign-up') {
-          const newUser = await signUp(data);
+            const userData = {
+                firstName: data.firstName!,
+                lastName: data.lastName!,
+                address1: data.address1!,
+                city: data.city!,
+                state: data.state!,
+                postalCode: data.postalCode!,
+                dateOfBirth: data.dateOfBirth!,
+                ssn: data.ssn!,
+                email: data.email,
+                password: data.password
+            }
+            
+            const newUser = await signUp(userData);
   
-          setUser(newUser);
+            setUser(newUser);
         }
   
         if(type === 'sign-in') {
@@ -83,7 +97,7 @@ const AuthForm = ({type}: {type: string}) => {
         </header>
         {user ? (
             <div className='flex flex-col gap-4'>
-
+                <PlaidLink user={user} variant="primary" />
             </div>
         ) : (
             <>
